2dac310edb78ea7d95164130a23bb2bab5be782f
YOUR NAME <rakiasegev@gmail.com>
2019-11-09 16:01:39 -0800

    updating about pages and images

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,18 @@ class About extends Component {
     return (
         <div>
           <h2>About</h2>
-        </div>
+								<p>Our Mission</p>
+								<h2>Environmental Awareness in a Click</h2>
+							<p>We utilize the massive quantity of open-source satellite data to track and analyze quantitatively how natural stuctures have changed over time. 
+								Through image analysis and machine learning platforms we analyze this and present it for you to explore. 
+								You can look at specific locations and through Satellite Raspberry track deforestation, climate change effects, and fires.</p>
+
+							<p>
+								Awareness is the first step to change. This project seeks to help individuals and scientists make educated decisions on nature conservation efforts and first responder action. 
+								This issue is of growing importance as the effects of climate change become more severe. We are here to help make a change.
+
+								We also would like to reference the open source platforms we use in case you want to explore them: Earth Engine.
+							</p></div>
     );
   }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import background from 'slide01.png';
 
 class Home extends Component {
   render() {
     return (
         <div>
-          <h2>Home</h2>
-        </div>
+          <img src={background}/>
+                <div class="container">
+                        <div class="row">
+                            <header id="header" class="alt">
+                              <div class="logo"><a href="/">Raspberry </a></div>
+                            </header>            
+                            <header>
+                                <p>Environmental Awareness in a Click</p>
+                                <h2>Satellite Raspberry</h2>
+                            </header>          </div>
+                        </div>
+                      </div>
     );
   }
 }
