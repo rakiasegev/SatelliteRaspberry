f864b8bab343e0922dcd8351e3edcbe53bccea26
Anisha Tandon <anisha1999@gmail.com>
2019-11-18 22:03:33 -0800

    dynamic querying works on console

--- src/components/news.js
+++ src/components/news.js
@@ -6,15 +6,22 @@ import SearchBar from 'material-ui-search-bar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import axios from 'axios';
 
-class News extends Component {
+class News extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: "world"
+    };
+  }
 
-  handleClick() {
+  handleClick(stateval) {
     // makes an API call to the getNews function 
+    // this.setState({v})
     axios.get('https://cors-anywhere.herokuapp.com/https://us-central1-cs121-1569887619031.cloudfunctions.net/getNews',
     {
       params: {
-      "newsItem":"fridge"
-    }
+      "newsItem": stateval
+      }
     })
     .then(response=> console.log(response))
   }
@@ -24,11 +31,10 @@ class News extends Component {
         <div>
           <h2>News</h2>
           <MuiThemeProvider>
-
           <SearchBar
-            onChange={() => console.log('onChange')}
-            onRequestSearch={() => this.handleClick()}
-            
+            value={this.state.value}
+            onChange={(newValue) => this.setState({value: newValue})}
+            onRequestSearch={() => this.handleClick(this.state.value)}
             style={{
             margin: '0 auto',
             maxWidth: 800
